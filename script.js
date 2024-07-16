a77744cb4daaa6fce4372d778889f723b08f5e97
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-07-16 21:06:11 +0530

    Features section done.

---
+++ script.js
@@ -0,0 +1,14 @@
+// const scrollers1 = document.querySelector('.scroller');
+
+// const addAnimation = () => {
+//     const cards = document.querySelectorAll('.scroller div');
+//     cards.forEach((item) => {
+//         const duplicatedItem = item.cloneNode(true);
+//         duplicatedItem.setAttribute("aria-hidden", true);
+//         scrollers1.appendChild(duplicatedItem);
+//     })
+// }
+
+// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+//     addAnimation();
+// }
